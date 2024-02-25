f916f349e43385d5595a75c37a944293c03a1329
Giulia Barber <julibarber22@gmail.com>
2024-02-25 16:29:51 -0300

    navbar responsive

--- app/ui/Navbar.jsx
+++ app/ui/Navbar.jsx
@@ -1,17 +1,26 @@
-import React from "react";
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
+'use client'
+import React, { useState } from "react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button } from "@nextui-org/react";
 import ShopIcon from "./icons/ShopIcon";
 import { spaceMono } from "./Fonts";
 export default function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
-    <Navbar shouldHideOnScroll maxWidth='full'>
-      <NavbarBrand justify="start">
-        <ShopIcon />
-        <p className={`${spaceMono.className} text-lg text-inherit ml-2`}>NFT Marketplace</p>
-      </NavbarBrand>
-      <NavbarContent className="flex" data-justify='end'>
-        <NavbarItem>
-          <Link color="foreground" href="#" className="text-lg">
+    <Navbar shouldHideOnScroll maxWidth='full' onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
+        <NavbarBrand justify="start">
+          <ShopIcon />
+          <p className={`${spaceMono.className} text-lg text-inherit ml-2`}>NFT Marketplace</p>
+        </NavbarBrand>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="md:hidden"
+        />
+      </NavbarContent>
+
+      <NavbarContent className="hidden md:flex gap-4" data-justify='end'>
+        <NavbarItem isActive>
+          <Link color="foreground" href="/marketplace" className="text-lg">
             Marketplace
           </Link>
         </NavbarItem>
@@ -31,6 +40,30 @@ export default function App() {
           </Button>
         </NavbarItem>
       </NavbarContent>
+
+      <NavbarMenu>
+        <NavbarMenuItem>
+          <Link color="foreground" href="/marketplace" className="text-lg w-full" size="lg">
+            Marketplace
+          </Link>
+        </NavbarMenuItem>
+        <NavbarMenuItem>
+          <Link color="foreground" href="/marketplace" className="text-lg w-full" size="lg">
+            Rankings
+          </Link>
+        </NavbarMenuItem>
+        <NavbarMenuItem>
+          <Link color="foreground" href="/marketplace" className="text-lg w-full" size="lg">
+            Connect a wallet
+          </Link>
+        </NavbarMenuItem>
+        <NavbarMenuItem>
+          <Button as={Link} color="secondary" href="#" className="text-lg w-full">
+            Sign Up
+          </Button>
+        </NavbarMenuItem>
+      </NavbarMenu>
+
     </Navbar>
   );
 }
