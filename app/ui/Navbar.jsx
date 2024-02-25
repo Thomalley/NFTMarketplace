2ba46bdb62ca9818833a86d0ee35704c4a367de5
Giulia Barber <julibarber22@gmail.com>
2024-02-24 21:14:01 -0300

    fuente en navbar

--- app/ui/Navbar.jsx
+++ app/ui/Navbar.jsx
@@ -1,32 +1,32 @@
 import React from "react";
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
 import ShopIcon from "./icons/ShopIcon";
-
+import { spaceMono } from "./Fonts";
 export default function App() {
   return (
     <Navbar shouldHideOnScroll maxWidth='full'>
       <NavbarBrand justify="start">
         <ShopIcon />
-        <p className="font-bold text-inherit ml-2">NFT Marketplace</p>
+        <p className={`${spaceMono.className} text-lg text-inherit ml-2`}>NFT Marketplace</p>
       </NavbarBrand>
       <NavbarContent className="flex" data-justify='end'>
         <NavbarItem>
-          <Link color="foreground" href="#">
+          <Link color="foreground" href="#" className="text-lg">
             Marketplace
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="#">
+          <Link color="foreground" href="#" className="text-lg">
             Rankings
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="#">
+          <Link color="foreground" href="#" className="text-lg">
             Connect a wallet
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Button as={Link} color="secondary" href="#">
+          <Button as={Link} color="secondary" href="#" className="text-lg">
             Sign Up
           </Button>
         </NavbarItem>
